9b36de7b57c933adf67e41bf75e0188e0c58c550
brian-pieces <brian@pieces.app>
2024-04-17 13:51:24 -0400

    fix old dll references

--- CLBlast/windows/lib/cmake/CLBlast/CLBlastConfig-release.cmake
+++ CLBlast/windows/lib/cmake/CLBlast/CLBlastConfig-release.cmake
@@ -8,13 +8,13 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "clblast" for configuration "Release"
 set_property(TARGET clblast APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(clblast PROPERTIES
-  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/clblast.lib"
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
   IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "C:/OpenCL/lib/OpenCL.lib"
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/clblast.dll"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/clblast.lib"
   )
 
 list(APPEND _cmake_import_check_targets clblast )
-list(APPEND _cmake_import_check_files_for_clblast "${_IMPORT_PREFIX}/lib/clblast.lib" "${_IMPORT_PREFIX}/bin/clblast.dll" )
+list(APPEND _cmake_import_check_files_for_clblast "${_IMPORT_PREFIX}/lib/clblast.lib" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
